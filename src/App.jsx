9fb6b64c05ce4de9cd2ca29838c89ae67346b0e7
aiveeKeiSoriano <aivee.soriano7@gmail.com>
2026-03-24 03:50:33 +0800

    project init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+          <h1>aiveekei</h1>
+      </section>
+
+      {/* <div className="ticks"></div>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section> */}
+    </>
+  )
+}
+
+export default App
