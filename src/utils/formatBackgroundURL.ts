7bab0edbc0bff9f6fc9bccb349db6c54430c3523
aiveeKeiSoriano <aivee.soriano7@gmail.com>
2026-04-20 18:51:30 +0800

    TASK: Add in-line comments to explain logics

--- src/utils/formatBackgroundURL.ts
+++ src/utils/formatBackgroundURL.ts
@@ -1,3 +1,9 @@
+/**
+ * I'm using this util for added background-image in styled-components
+ * Because the string parsing of styled-components is having a problem with quotation
+ * marks for image url
+ */
+
 const formatCssUrl = (url: string) => {
   return `url("${url}")`;
 };
